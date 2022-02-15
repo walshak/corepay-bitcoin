7ee6c619fae03c84a352a80bea382d615ad0ecd6
Walshak Timothy Apollos <walshak1999@gmail.com>
2022-02-15 16:51:03 +0000

    ditched the python-bicoinlib library, used pure rpc commands

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask, request, jsonify
+from cv2 import subtract
+from flask import Flask, request, jsonify, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from bitcoinlib.wallets import Wallet, wallet_delete
-from bitcoinlib.services.bitcoind import BitcoindClient
-from bitcoinlib.services import authproxy
 from bitcoinlib.mnemonic import Mnemonic
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 import os
 
-#global vars
-db_uri = 'mysql://corepay:18781875Core@localhost:3306/corepay_btc'
-rpc = authproxy.AuthServiceProxy("http://%s:%s@127.0.0.1:18332"%("corepay","18781875Core"))
+from markupsafe import re
 
-# base_url = 'http://user:password@server_url:18332'
-# bdc = BitcoindClient(base_url=base_url)
-# txid = 'e0cee8955f516d5ed333d081a4e2f55b999debfff91a49e8123d20f7ed647ac5'
-# rt = bdc.getrawtransaction(txid)
-# print("Raw: %s" % rt)
+#global vars
+db_uri = 'mysql://root:18781875Core@localhost:3306/corepay_btc'
+
+base_url = 'http://corepago:18781875Core@localhost:18332'
+bdc = AuthServiceProxy(base_url)
+wallet_name_default = 'hkmnnjjmmkmkk'
+passphrase_default = "just anchor glance brown person liquid pair joy word clip effort broccoli"
+#txid = 'e0cee8955f516d5ed333d081a4e2f55b999debfff91a49e8123d20f7ed647ac5'
+#rt = bdc.getrawtransaction(txid)
+#print("Raw: %s" % rt)
 
 # init app
 app = Flask(__name__)
@@ -25,54 +27,283 @@ app = Flask(__name__)
 
 @app.route('/wallet/create', methods=['POST'])
 def create_wallet():
-    owner = request.json['owner']
-    name = request.json['name']
-    # create a Mnemonic
+    global bdc
+    
+    #get posted wallet name
+    wallet_name = request.json['wallet_name']
+    
+    # create a Mnemonic phrase/masterkey
     passphrase = Mnemonic().generate()
-    w = Wallet.create(name, keys=passphrase, network='testnet', db_uri=db_uri, owner=owner)
-    w = Wallet(name,db_uri=db_uri)
-    return jsonify({'passphrase':passphrase,'info':w.as_json()})
-
+    
+    try:
+        #create wallet
+        bdc.createwallet(wallet_name,False, False, passphrase)
+        
+        try:
+            #reload wallet
+            bdc.unloadwallet(wallet_name)
+            bdc.loadwallet(wallet_name)
+            
+            try:
+                #add wallet precision to bdc base_url so that getwalletinfo() will work
+                bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+                try:
+                    #get wallet info
+                    info = bdc.getwalletinfo()
+                    
+                    #format and snd response
+                    return jsonify({'passphrase':passphrase,'info':info})
+                except JSONRPCException as e:
+                    return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+            except JSONRPCException as e:
+                return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+     
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    
+       
 @app.route('/wallet/balance',methods=['GET'])
 def get_balance():
     wallet_name = request.json['wallet_name']
-    # open the wallet
-    w = Wallet(wallet_name,db_uri=db_uri)
 
-    # scan for uxto's
-    w.scan()
+    #set wallet_name
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            # get new wallet address
+            bal = bdc.getbalance()
+            bal_details = bdc.getbalances()
+            #use the key to create a payment address
+            return jsonify(balance = bal, bal_details = bal_details)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+@app.route('/wallet/get-wallet-info', methods=['GET'])
+def get_wallet_info():
+    global bdc
+    global wallet_name_default
+
+    wallet_name = request.json['wallet_name']
+
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
     
-    return jsonify({'balance': w.balance()})
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            # get the wallet address info
+            info = bdc.getwalletinfo()
+
+            #return info about the given wallet
+            return jsonify(info)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
 
 
 @app.route('/wallet/get-address', methods=['GET'])
 def get_address():
-    wallet_name = request.json['wallet_name']
-    #open the wallet
-    w = Wallet(wallet_name,db_uri=db_uri)
-    # get the wallet key
-    key = w.get_key()
+    global bdc
+    global wallet_name_default
 
-    #use the key to create a payment address
-    return jsonify({'address':key.address})
+    wallet_name = request.json['wallet_name']
+    label = request.json['label']
+
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            # get new wallet address
+            address = bdc.getnewaddress(label)
+            address = bdc.getaddressinfo(address)
+            #use the key to create a payment address
+            return jsonify(address)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+@app.route('/wallet/get-address-info', methods=['GET'])
+def get_address_info():
+    global bdc
+    global wallet_name_default
+
+    wallet_name = request.json['wallet_name']
+    address = request.json['address']
+
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            # get the wallet address info
+            info = bdc.getaddressinfo(address)
+
+            #return info about the given address
+            return jsonify(info)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+@app.route('/wallet/dump-wallet-info', methods=['GET'])
+def dump_wallet_info():
+    global bdc
+    global wallet_name_default
+
+    wallet_name = request.json['wallet_name']
+    filename = request.json['filename']
+    passphrase = request.json['passphrase']
+
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+    
+    #set default passphrase 
+    if passphrase == "":
+        passphrase = passphrase_default
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            #use the passphrase to unlock the wallet
+            bdc.walletpassphrase(passphrase,120)
+            try:
+                # get the wallet address info and dump in a file
+                info = bdc.dumpwallet('/home/walshak/'+filename)
+
+                #return info about the given wallet
+                return jsonify(info)
+            except JSONRPCException as e:
+                return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+            
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+    
+    
+    
 
 @app.route('/wallet/send', methods=['POST'])
 def send_to():
     wallet_name = request.json['wallet_name']
     reciever_address = request.json['reciever_address']
-    #open wallet
-    w = Wallet(wallet_name,db_uri=db_uri)
-    t = t = w.send_to(reciever_address, '0.000 tBTC',network='testnet',offline=True)
-    return jsonify({t.info()})
+    amount = request.json['amount']
+    rx_name = request.json['reciever_name']
+    tx_desc = request.json['description']
+    subtract_fee = request.json['subtract_fee']
+    passphrase = request.json['passphrase']
 
-@app.route('/wallet/tx/satus',methods=['GET'])
+    #open wallet
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+
+    if passphrase == "":
+        passphrase = passphrase_default
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+
+        try:
+            bdc.walletpassphrase(passphrase,120)
+
+            try:
+                # send
+                tx_id = bdc.sendtoaddress(reciever_address,amount,tx_desc,rx_name,subtract_fee)
+                # get the wallet tx info
+                tx = bdc.gettransaction(tx_id,True,True)
+                #return info about the tx 
+                return jsonify({'tx':tx})
+            except JSONRPCException as e:
+                return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+    
+
+@app.route('/wallet/tx/status',methods=['GET'])
 def tx_status():
-    # wallet = request.json['wallet']
-    address = request.json['address']
-    #rescan blockchain
-    if rpc.rescanblockchain():
-        last_tx = rpc.listrecievedbyaddress(1,True,address=address)
-        return last_tx
+    wallet_name = request.json['wallet_name']
+    tx_id = request.json['tx_id']
+    
+    #open wallet
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+        try:
+            # get the wallet tx info
+            tx = bdc.gettransaction(tx_id,True,True)
+
+            #return info about the given tx
+            return jsonify(tx)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+@app.route('/wallet/tx/list',methods=['GET'])
+def tx_list():
+    wallet_name = request.json['wallet_name']
+    label = request.json['label']
+    count = request.json['count']
+    skip = request.json['skip']
+    
+    #open wallet
+    #set the name of the wallet
+    if wallet_name == "":
+        wallet_name = wallet_name_default
+    #return wallet_name
+    
+    try:
+        #add wallet precision to bdc base_url so that getnewaddress() will work
+        bdc = AuthServiceProxy(base_url+'/wallet/'+wallet_name)
+        try:
+            # get the wallet address info
+            tx = bdc.listtransactions(label,count,skip)
+
+            #return info about the given tx
+            return jsonify(tx)
+        except JSONRPCException as e:
+            return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+    except JSONRPCException as e:
+        return Response('Error: {}'.format(str(e)), status=500, mimetype='application/json')
+
+
 # start server
 if __name__ == '__main__':
     app.run(debug=True)
